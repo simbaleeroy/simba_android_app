073f7a28938e7e55c5b0dda938872a0af58dcbc0
simbaleeroy <89958513+simbaleeroy@users.noreply.github.com>
2021-12-30 19:18:01 -0700

    Initial commit

---
+++ app/src/main/java/com/example/simba_android_app/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.simba_android_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) findViewById(R.id.text)).setTextColor(getResources().getColor(R.color.white));
+            }
+
+        });
+
+    }
+}
